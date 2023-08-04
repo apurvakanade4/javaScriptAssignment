46f2877d76944d494ae6d26eda11f6087bc4d8e0
apurvakanade4 <apurvakanade4@gmail.com>
2023-08-04 23:23:13 +0530

    01.variableAssign.js
    01.variableAssign.js

---
+++ 01.variable_assign.js
@@ -0,0 +1,33 @@
+var myFullName;
+console.log("Before Initialization:", myFullName);
+
+var myFullName = "Apurva satpute";
+console.log("After Initialization:", myFullName);
+
+var myFullName = "Apurva saket satpute";
+console.log("After Update:", myFullName);
+console.log("-------------------------");
+
+var myLove = "I Love Only JS";
+console.log(myLove);
+console.log("-------------------------");
+var fistName = "Apurva";
+console.log("fistName:",fistName); 
+
+var lastName = "satpute";
+console.log("lastName:", lastName); 
+
+var age = "27";
+console.log("age:", age); 
+
+var pinCode = "411038";
+console.log("pinCode:", pinCode); 
+
+var address = "vishrantwadi";
+console.log("address:", address); 
+
+var gender = "Female";
+console.log("gender:", gender); 
+
+var isMarried = "Yes";
+console.log("isMarried:", isMarried); 
